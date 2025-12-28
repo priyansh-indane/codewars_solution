d42f64406ba78cb50b750fbae016f0b692ee02f8
Priyansh Indane <102822316+priyansh-indane@users.noreply.github.com>
2025-12-28 10:08:40 +0530

    Implement function to calculate average of numbers

---
+++ 8kyu_Calculate average.py
@@ -0,0 +1,10 @@
+def find_average(numbers):
+    total_sum=sum(numbers)
+    total_numbers=len(numbers)
+    
+    if(total_numbers>0):
+        return (total_sum/total_numbers)
+    
+    else:
+        return 0
+        
