6180ed7491c8ae1006e918b044edf9e257590bdf
Priyansh Indane <102822316+priyansh-indane@users.noreply.github.com>
2026-01-01 10:31:33 +0530

    Implement function to remove every other element

---
+++ 8kyu_Removing_elements.py
@@ -0,0 +1,10 @@
+def remove_every_other(my_list):
+    ls=[]
+    for i, x in enumerate(my_list):
+        if(i%2==0):
+            ls.append(x)
+            
+        else:
+            pass
+        
+    return ls  
