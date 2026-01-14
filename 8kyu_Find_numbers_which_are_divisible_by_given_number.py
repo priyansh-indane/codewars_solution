6094a3bb0540783c329466c135c62c41104ddb44
Priyansh Indane <102822316+priyansh-indane@users.noreply.github.com>
2026-01-14 14:51:42 +0530

    Add function to find numbers divisible by a divisor

---
+++ 8kyu_Find_numbers_which_are_divisible_by_given_number.py
@@ -0,0 +1,9 @@
+def divisible_by(numbers, divisor):
+    ls=[]
+    for x in numbers:
+        if(x%divisor==0):
+            ls.append(x)
+            
+        else:
+            pass
+    return ls   
