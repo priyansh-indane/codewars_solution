5bb319b00748704d473613f2dc8ff178b25d8d18
Priyansh Indane <102822316+priyansh-indane@users.noreply.github.com>
2026-05-15 10:15:16 +0530

    Add function to correct Polish letters in string

---
+++ 8kyu_Polish_alphabet.py
@@ -0,0 +1,8 @@
+def correct_polish_letters(st):
+    replace = {
+        'ą': 'a', 'ć': 'c', 'ę': 'e',
+        'ł': 'l', 'ń': 'n', 'ó': 'o',
+        'ś': 's', 'ź': 'z', 'ż': 'z'
+    }
+    
+    return ''.join(replace.get(x, x) for x in st)
