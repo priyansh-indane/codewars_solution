1d0866c44845e25bd4116eb57b882533cd9cbf5a
Priyansh Indane <102822316+priyansh-indane@users.noreply.github.com>
2026-06-06 09:47:20 +0530

    Add cooking_time function for boiling eggs

---
+++ 7kyu_Boiled_Eggs.py
@@ -0,0 +1,12 @@
+import math
+def cooking_time(eggs):
+    
+    if(eggs==0):
+        return 0
+    
+    elif(eggs<=8):
+        return 5
+    
+    else:
+        return math.ceil(eggs/8)*5
+    
