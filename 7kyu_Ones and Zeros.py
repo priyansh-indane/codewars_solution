66d83b422b8bfa6a8f570c3889c2ed672cbb2751
Priyansh Indane <102822316+priyansh-indane@users.noreply.github.com>
2026-06-16 13:09:26 +0530

    Create 7kyu_Ones and Zeros.py

---
+++ 7kyu_Ones and Zeros.py
@@ -0,0 +1,9 @@
+def binary_array_to_number(arr):
+    
+    convert_to_str="".join(str(x) for x in arr)
+    
+    convert_to_deci=int(convert_to_str,2)
+    
+    return convert_to_deci
+    
+    
